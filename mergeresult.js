13b3d3b5510505b8f511911672460725a60b22a9
y-i <y-i@users.noreply.github.com>
2018-05-27 23:44:25 +0900

    update merge script and poc

--- mergeresult.js
+++ mergeresult.js
@@ -1,11 +1,23 @@
 const chrome66 = require('./result/Chrome%2F66.json');
 const chrome68 = require('./result/Chrome%2F68.json');
+const ff58 = require('./result/Firefox%2F58.json');
 const ff59 = require('./result/Firefox%2F59.json');
+const ff60 = require('./result/Firefox%2F60.json');
 const ff61 = require('./result/Firefox%2F61.json');
 
 const fs = require('fs');
 
 const result = {};
+
+const pushRow = (data, name) => {
+    Object.keys(data).forEach((k1) => {
+        Object.keys(data[k1]).forEach((k2) => {
+            Object.keys(data[k1][k2]).forEach((k3) => {
+                result[k1][k2][k3][name] = data[k1][k2][k3];
+            });
+        });
+    });
+};
 
 Object.keys(chrome66).forEach((k1) => {
     result[k1] = {};
@@ -18,28 +30,11 @@ Object.keys(chrome66).forEach((k1) => {
     });
 });
 
-Object.keys(chrome68).forEach((k1) => {
-    Object.keys(chrome68[k1]).forEach((k2) => {
-        Object.keys(chrome68[k1][k2]).forEach((k3) => {
-            result[k1][k2][k3]['chrome/68'] = chrome68[k1][k2][k3];
-        });
-    });
-});
+pushRow(chrome68, 'chrome/68');
 
-Object.keys(ff59).forEach((k1) => {
-    Object.keys(ff59[k1]).forEach((k2) => {
-        Object.keys(ff59[k1][k2]).forEach((k3) => {
-            result[k1][k2][k3]['firefox/59'] = ff59[k1][k2][k3];
-        });
-    });
-});
-
-Object.keys(ff61).forEach((k1) => {
-    Object.keys(ff61[k1]).forEach((k2) => {
-        Object.keys(ff61[k1][k2]).forEach((k3) => {
-            result[k1][k2][k3]['firefox/61'] = ff61[k1][k2][k3];
-        });
-    });
-});
+pushRow(ff58, 'firefox/58');
+pushRow(ff59, 'firefox/59');
+pushRow(ff60, 'firefox/60');
+pushRow(ff61, 'firefox/61');
 
 fs.writeFileSync('web/result.json', JSON.stringify(result));
